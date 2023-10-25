f17060b7560cb989e6f5fddaeac2341a42d15a31
Kathryn Aman <kathryn.aman@gmail.com>
2023-10-25 21:46:55 +0000

    Changing Skeleton Project

--- Makefile
+++ Makefile
@@ -1,11 +1,7 @@
 .PHONY: clean
 
 clean: 
-	rm derived_data/*
 	rm figures/*
 	
-derived_data/Sleep_health_and_lifestyle_dataset.csv:
-	curl -Lo derived_data/Sleep_health_and_lifestyle_dataset.csv https://raw.githubusercontent.com/Kathrynaman/BIOS611-Final-Project/main/derived_data/Sleep_health_and_lifestyle_dataset.csv
-	
-figures/figure1.png figures/figure2.png: derived_data/Sleep_health_and_lifestyle_dataset.csv
+figures/figure1_age_hist.png figures/figure2_sleepduration_boxplot.png: source_data/sleep_health_and_lifestyle_dataset.csv Skratch.r
 	Rscript Skratch.r
--- Skratch.r
+++ Skratch.r
@@ -1,11 +1,11 @@
 setwd(getwd())
 library(tidyverse)
 
-data1 <- read_csv("derived_data/Sleep_health_and_lifestyle_dataset.csv")
+data1 <- read_csv("source_data/sleep_health_and_lifestyle_dataset.csv")
 
 figure_1 <- ggplot(data1) + geom_histogram(aes(x=Age))
-ggsave(filename="figures/figure1.png", plot=figure_1)
+ggsave(filename="figures/figure1_age_hist.png", plot=figure_1)
 
 
 figure_2 <- ggplot(data1) + geom_boxplot(aes(x=`Sleep Duration`))
-ggsave(filename="figures/figure2.png", plot=figure_2)
+ggsave(filename="figures/figure2_sleepduration_boxplot.png", plot=figure_2)
